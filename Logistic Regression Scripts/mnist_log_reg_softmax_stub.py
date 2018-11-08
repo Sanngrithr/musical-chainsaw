c45d4459c66217e005c13ed3f4a10dafd123fb8b
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2018-11-08 12:06:18 +0100

    Add files via upload

---
+++ Logistic Regression Scripts/mnist_log_reg_softmax_stub.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 16:22:23 2018
+
+@author: LaRu
+"""
+
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import tensorflow as tf
+
+tf.reset_default_graph()
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+#print(mnist.train.images.shape)
+#print(mnist.train.labels.shape)
+mask_0 = mnist.train.labels == 0
+mask_1 = mnist.train.labels == 1
+zeros = tf.boolean_mask(mnist.train.images, mask_0)
+labels_0 = tf.boolean_mask(mnist.train.labels, mask_0)
+ones = tf.boolean_mask(mnist.train.images, mask_1)
+labels_1 = tf.boolean_mask(mnist.train.labels, mask_1)
+all0_1 = tf.concat([zeros, ones], 0)
+all0_1_labels = tf.cast(tf.concat([labels_0, labels_1], 0), tf.int32)
+
+#import Data with tf.data.Dataset.from_tensor_slices
+dataset = tf.data.Dataset.from_tensor_slices((all0_1,all0_1_labels))
+# Shuffle, repeat, and batch the examples.
+dataset = dataset.shuffle(12000).repeat(10).batch(10)
+iterator = dataset.make_initializable_iterator()
+img_input, labels = iterator.get_next()
+print(img_input, labels)
+
+#define the parameters variable (we use W instead of thetas) of shape 784x2 of type tf.float32
+#use initializer = tf.contrib.layers.xavier_initializer()
+W = tf.get_variable(TO DO)
+#define bias variable of shape 2, type float, initializer = tf.constant_initializer(0.0001)
+b = tf.get_variable(TO DO)
+
+#compute Input * Parameters + bias (see slide 22 (bias term omitted there))
+#==============================================================================
+# Logit numerical example (not in python syntax) but pseudocode
+# One sample with 5 features, W (theta) parameters 
+# [1 2 3 4 5]*[0.1 0.5   + [0.01 0.05] = [5.41 ]
+#              0.3 0.2
+#              0.2 0.8
+#              0.9 0.6
+#              0.1 0.4]
+#==============================================================================
+logits = 
+# checkout tf.losses.sparse_softmax_cross_entropy
+cross_entropy = 
+
+#compute the prediction by looking if the index of the maximum in the logits equals the label
+#checkout tf.argmax, tf.equal and you also need to convert the index from float to tf.int32
+correct_prediction = 
+#compute the accuracy by computing the mean of the correct_prediction
+#checkout tf.reduce_mean
+accuracy = 
+#use the minimize function of the in-built Gradient descent optimizer with a learning rate that you need to find out what would work
+opt = 
+
+############test dataset####################################################
+mask_0_t = mnist.test.labels == 0
+mask_1_t = mnist.test.labels == 1
+zeros_t = tf.boolean_mask(mnist.test.images, mask_0_t)
+labels_0_t = tf.boolean_mask(mnist.test.labels, mask_0_t)
+ones_t = tf.boolean_mask(mnist.test.images, mask_1_t)
+labels_1_t = tf.boolean_mask(mnist.test.labels, mask_1_t)
+all0_1_t = tf.concat([zeros_t, ones_t], 0)
+all0_1_labels_t = tf.cast(tf.concat([labels_0_t, labels_1_t], 0), tf.int32)
+
+dataset_t = tf.data.Dataset.from_tensor_slices((all0_1_t,all0_1_labels_t))
+#one shot iterator, iterates only once through the data
+iterator_t = dataset_t.make_one_shot_iterator()
+img_input_t, labels_t = iterator_t.get_next()
+#need to put input in format(batchsize, dimension), alternatively one can use dataset_t = dataset_t.batch(1) BEFORE one shot iterator
+img_input_t = tf.expand_dims(img_input_t,0)
+print(img_input_t, labels_t)
+logits_t = 
+correct_prediction_t = 
+accuracy_t = 
+
+j = 0
+total_acc = 0.0
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    #iterator should be initialized
+    sess.run(iterator.initializer)
+    #run training
+    for i in range(10):
+        ce, acc, _ = sess.run([cross_entropy, accuracy, opt])
+        print(ce, acc)
+    #run test, we used a one shot iterator therefor no initialization is needed
+    try:
+        while True:
+                cp, acc = sess.run([correct_prediction_t, accuracy_t]) 
+                total_acc += acc
+                j += 1
+    except tf.errors.OutOfRangeError:
+        pass
+    all_ = sess.run(all0_1)
+    print(all_.shape)
+print(total_acc/j)
+sess.close()   
---
+++ Logistic Regression Scripts/mnist_py_logreg_stub.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 14:13:19 2018
+
+@author: floko
+"""
+
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np    
+
+"""
+Hyperparams
+"""
+learning_rate = 0.0001
+#number iterations for gradient descent (gdc)
+gdc_steps = 4
+    
+"""
+Data Import & Preprocessing
+"""
+# import dataset
+mnist = input_data.read_data_sets("./mnist_data/", one_hot=False)
+
+def data_preprocess(images, labels):
+
+    # number of examples m  
+    m = images.shape[0]
+    
+    # create vector of ones to concatenate to our data matrix (for intercept terms)
+    ones = np.ones(shape=[m, 1])
+    images = np.concatenate((ones, images), axis=1)
+    
+    # to retrieve the images and corresponding labels where the label is either 0 or 1, 
+    # we define two logical vectors that can be used to subset our data_matrices
+    logical_mask_0 = labels == 0
+    logical_mask_1 = labels == 1
+    
+    images_zeros = images[logical_mask_0]
+    labels_zeros = labels[logical_mask_0]
+    images_ones = images[logical_mask_1]
+    labels_ones = labels[logical_mask_1]
+    
+    X = np.concatenate((images_zeros, images_ones), axis=0)
+    y = np.concatenate((labels_zeros, labels_ones), axis=0)
+    
+    # shuffle the data and corresponding labels in unison
+    def _shuffle_in_unison(a, b):
+        assert len(a) == len(b)
+        p = np.random.permutation(len(a))
+        return a[p], b[p]
+
+    return _shuffle_in_unison(X,y)   
+
+
+
+# get train and test datasets
+dataset_train = mnist.train
+dataset_test = mnist.test
+
+X,y = data_preprocess(dataset_train.images, dataset_train.labels)
+
+
+"""
+Logistic Regression Model and Grdient Descent (GDC) optimization
+"""
+# number of features n
+n = X.shape[1]
+# we need to define our model parameters to be learned. we use W (weights) instead of theta this time.
+mu, sigma = 0, 0.01 # mean and standard deviation
+w = np.random.normal(mu, sigma, n)
+
+# define sigmoid nonlinearity, Foliennr.
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+#implement equation 35 in the slides of Lecture_2
+#y_hat is h_theta(X) in equation 35
+#y and y_hat are vectors in the dimension of training samples
+def compute_cross_entropy_loss(y, y_hat):
+    return -(np.sum((y * np.log10(y_hat)) + (1 - y) * np.log10(1 - y_hat)))
+
+
+print("_______________________________")
+print("Starting optimization on training set")
+
+for step in range(0, gdc_steps):
+    print("Performing step " + str(step) + " of gradient descent.")
+    # compute X*theta (eq 1 in the slides), here we denote the params not theta but w
+    z = np.dot(X,w)   
+    # apply the sigmoid nonlinearity
+    y_hat = sigmoid(z)
+    
+    loss = compute_cross_entropy_loss(y, y_hat)
+    print("Loss at step " + str(step) + ": " + str(loss))
+    
+    # compute the error term, i.e. the difference between labels and estimated labels y_hat, see equation 24 in the slides 
+    error_term = np.dot(np.transpose(z - y),X)
+    #print(error_term.size)
+    
+    # compute the gradient. as our data matrix X is currently layed out as X_j_i, we got to transpose it
+    # see derived formula of the gradient calculation
+    gradients = np.dot(np.transpose(X),(y_hat - y))
+    #print(X.shape)
+    #print(error_term.shape)
+    #print(gradients.shape)
+    
+    # update w using the gdc update rule
+    w = w-learning_rate*gradients
+    
+    # compute the predictions, i.e. whenever y hat is greater or equal to 0.5 we get class 1
+    # and cast them to int values
+    predictions = np.around(sigmoid(np.dot(X,w)))
+    
+    #print(int(predictions==y))
+    # compute mean accuracy by looking if the predictions match the labels y and applying the np.mean function to the result
+    accuracy = np.mean(predictions == y)
+    print("Accuracy at step " + str(step) + ": " + str(accuracy))
+    
+    
+"""
+Evaluation of test set with the trained logred model
+"""
+print("_______________________________")
+print("Starting evaluation of test set")
+
+X,y = data_preprocess(dataset_test.images, dataset_test.labels)
+z = np.dot(X,w)
+y_hat = sigmoid(z)
+predictions = np.around(sigmoid(np.dot(X,w)))
+accuracy = np.mean(predictions == y)
+print("Accuracy of test set: " + str(accuracy))
