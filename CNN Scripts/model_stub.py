869465b9e2cabb8ed3099c93022a2be69f50e0ad
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2018-12-11 16:29:14 +0100

    Add files via upload

--- CNN Scripts/model_stub.py
+++ CNN Scripts/model_stub.py
@@ -33,28 +33,48 @@ def bias_variable(shape):
 def build_model(input_data, label, train_mode, keep_prob, learning_rate, batch_size):
 
     #Initialize weights for the convolutions
-    W1 = weight_variable([3,3,1,32])
+    W1 = weight_variable([3,3,3,32])#dimensions wrong
     b1 = bias_variable([32])
     
-    W2 = weight_variable([3,3,1,32])
+    W2 = weight_variable([3,3,32,32])#dimensions wrong
     b2 = bias_variable([32])
     
-    W3 = weight_variable([3,3,1,64])
+    W3 = weight_variable([3,3,32,64])#dimensions wrong
     b3 = bias_variable([64])
 
 
     #Execute the first two convolutions
-    h_conv1 = conv2d(input_data, W1, b1, 0)
-    h_conv2 = conv2d(h_conv1, W2, b2, 0)
+    h_conv1 = conv2d(input_data, W1, b1, 1)
+    h_conv2 = conv2d(h_conv1, W2, b2, 1)#shape: (?,32,32,32)
 
     #Create a pooling layer
-    h_pool1 = max_pool_2x2(h_conv2)
+    h_pool1 = max_pool_2x2(h_conv2)#shape: (?,16,16,32)
+    print(h_pool2.shape)
 
     #Thrid convolution
-    h_conv3 = conv2d(h_pool1, W3, b3, 0)
+    h_conv3 = conv2d(h_pool1, W3, b3, 1)#shape: (?,16,16,64)
 
     #Another pooling layer
-    h_pool2 = max_pool_2x2(h_conv3)
+    h_pool2 = max_pool_2x2(h_conv3)#shape: (?,8,8,64)
+    print(h_pool2.shape) 
+
+    #Fully connected layers start here
+    #Initialize Weights for fully connected layer
+    W1_fc = weight_variable([8*8*64,512])
+    b1_fc = bias_variable([512])
+
+    W2_fc = weight_variable([512,10])
+    b2_fc = bias_variable([10])
+
+    #reshape h_pool2 to a vector
+    h_pool2_vec = tf.reshape(h_pool2,[-1,8*8*64])
+
+    #Fully connected layers fc1 and fc2
+    #Maybe the activation function needs to be used again, not entirely sure though
+    fc1 = tf.matmul(h_pool2_vec,W1_fc)
+    fc2 = tf.matmul(fc1, W2_fc)
+
+    
 
         
     
