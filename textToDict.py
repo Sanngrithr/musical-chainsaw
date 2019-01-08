b8134527e063e3e0da69f323e1706f6be0ea3f0f
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2019-01-08 18:07:33 +0100

    save our awesome script

---
+++ textToDict.py
@@ -0,0 +1,81 @@
+import os
+
+eng_index = 0
+ger_index = 0
+
+#dictionaries with all key value pairs(contains duplicate values, unique keys)
+english_dict = {}
+german_dict = {}
+
+#final dictionaries without duplicates
+eng_dict_clean = {}
+ger_dict_clean = {}
+
+
+for filename in os.listdir('text2text'):
+    file = open('text2text/' + filename)
+    #con file structure:
+    #annot_eng	SATURDAY EVENING CINEMA X-right START WHEN?
+    #annot_deu	SAMSTAG ABEND KINO X-rechts ANFANGEN WANN?
+    #transl_eng	When does the film start on Saturday night?
+    #transl_deu	Wann fängt am Samstag Abend das Kino an?
+
+    #iso file structure
+    #annot_eng	START
+    #annot_deu	ANFANGEN
+
+    if 'con' in filename:
+        #operations to split the strings read from the file and clean them up
+        lines = file.readlines()
+        eng1 = lines[2]
+        ger1 = lines[3]
+        engwords = eng1.split()
+        gerwords = ger1.split()
+        engwords.remove('transl_eng')
+        gerwords.remove('transl_deu')
+    
+        #remove punctuation
+        engwords[len(engwords)-1] = engwords[len(engwords)-1][0:engwords[len(engwords)-1].count('')-2]
+        gerwords[len(gerwords)-1] = gerwords[len(gerwords)-1][0:gerwords[len(gerwords)-1].count('')-2]
+
+    if 'iso' in filename:
+        lines = file.readlines()
+        eng1 = lines[0]
+        ger1 = lines[1]
+        
+        engwords = eng1.split()
+        engwords = engwords[1].split('|')
+        gerwords = ger1.split()
+        gerwords = gerwords[1].split('|')
+        
+    for word in engwords:
+        english_dict[eng_index] = word.lower()
+        eng_index = eng_index +1
+
+    for word in gerwords:
+        german_dict[ger_index] = word.lower()
+        ger_index = ger_index +1
+
+
+#remove duplicate values
+for key,value in english_dict.items():
+    if value not in eng_dict_clean.values():
+        eng_dict_clean[key] = value
+        
+for key,value in german_dict.items():
+    if value not in ger_dict_clean.values():
+        ger_dict_clean[key] = value
+
+
+#create inverse dictionaries
+english_inv_dict = {value:key for key,value in eng_dict_clean.items()}
+german_inv_dict = {value:key for key,value in ger_dict_clean.items()}
+
+
+print(len(ger_dict_clean))
+print(eng_dict_clean)
+
+##use string.lower() to turn a given string into lower case characters##
+
+###dict.update(dict2)
+###Adds dictionary dict2's key-values pairs to dict
